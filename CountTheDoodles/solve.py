e9e70ced8a102ee00b64298731f3237dbd9706ba
m0kr4n3 <jm_abdelmalek@esi.dz>
2020-11-14 00:10:33 +0100

    DoodlesCorrectOne

--- CountTheDoodles/solve.py
+++ CountTheDoodles/solve.py
@@ -1,19 +1,17 @@
 from requests import get
 from json import loads,dumps
-from re import match, IGNORECASE
 
 counter=0
 
 for year in range(2015,2020):
     for month in range(1,13):
-        url = f"https://www.google.com/doodles/json/{year}/{month}"
+        url = f"https://www.google.com/doodles/json/{year}/{month}?full=1"
         r=get(url=url).text
         d=loads(r)
         for i in reversed(d):
-            
-            if (match("algeri",i["name"],IGNORECASE) is not None) or (match("الجزائر",i["name"],IGNORECASE) is not None):
+            if "Algeria" in i["countries"]:
                 counter+=1
-                print("It had been made visible in {}".format(i["run_date_array"]))
+                print("It had been made visible in {}  Counter = {}".format(i["run_date_array"],counter))
 
 print("The number of Google Doodles that had been made visible in Algeria in the period (01-01-2015 - 01-01-2020) is {}".format(counter))
 
